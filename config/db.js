356f9438eb85f3d72cef8456904ad7cd148b97fb
kourakodeur <alhassaneb217@gmail.com>
2022-06-22 03:56:38 +0000

    Connection to DB with the ORM Sequelize

--- config/db.js
+++ config/db.js
@@ -1,11 +1,30 @@
-const mysql      = require('mysql');
-const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'wecco',
-  password : 'passer',
-  database : 'express'
-});
+// const mysql      = require('mysql');
+// const connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'wecco',
+//   password : 'passer',
+//   database : 'express'
+// });
  
-connection.connect();
+// connection.connect();
 
-module.exports=connection;
+// module.exports=connection;
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize('wecco', 'wecco', 'passer', {
+  host: 'localhost',
+  dialect: 'mysql',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,     
+    min: 0,     
+    idle: 10000
+  }
+});
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.customers = require('../models/Customer')(sequelize, Sequelize);
+module.exports = db;
